fbb0c3e8286f0ca2f5501579796941fdbb319f29
NataliaShamurova <shamurovann@yandex.ru>
2024-07-06 22:25:26 +0300

    Add files via upload

--- module_6_1.py
+++ module_6_1.py
@@ -1,52 +1,43 @@
 class Animal:
-    def __init__(self, name):
-        self.alive = True  # живой
-        self.fed = False  #накормленный
-        self.name = name
+    alive = True  # живой
+    fed = False  #накормленный
 
-    def __str__(self):
-        return f'{self.name}'
-
-
-class Mammal(Animal):
     def eat(self, food):
-        if isinstance(food, Plant):
-            if food.edible:
-                print(f'{self.name} съел {food.name}')
-                self.fed = True
-            else:
-                print(f'{self.name} не стал есть {food.name}')
-                self.alive = False
+
+        if food.edible:
+            print(f'{self.name} съел {food.name}')
+            self.fed = True
+        else:
+            print(f'{self.name} не стал есть {food.name}')
+            self.alive = False
+class Mammal(Animal):
+    def __init__(self, name):
+        self.name = name
+    # def __str__(self):
+    #     return f'{self.name}'
 
 
 class Predator(Animal):
-    def eat(self, food):
-        if isinstance(food, Plant):
-            if food.edible:
-                print(f'{self.name} съел {food.name}')
-                self.fed = True
-            else:
-                print(f'{self.name} не стал есть {food.name}')
-                self.alive = False
+    def __init__(self, name):
+        self.name = name
+
+    # def __str__(self):
+    #     return f'{self.name}'
 
 
 class Plant:
     edible = False
 
-    def __init__(self, name):
-        
-        self.name = name
-
-    def __str__(self):
-        return f'{self.name}'
-
 
 class Flower(Plant):
-    edible = False
-
+    def __init__(self, name):
+        self.name = name
 
 class Fruit(Plant):
     edible = True
+
+    def __init__(self, name):
+        self.name = name
 
 
 a1 = Predator('Волк с Уолл-Стрит')
